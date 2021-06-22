aed78d06a56f40c420b73009eb94d21461018b96
zhangliquan <zhangliquan@ebsig.com>
2021-06-22 11:55:11 +0800

    first commit code

---
+++ partone/src/main/java/com/zlq/partone/PartOneMainActivity.kt
@@ -0,0 +1,11 @@
+package com.zlq.partone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PartOneMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_part_one_main)
+    }
+}
